8e364f1db11c1b5295098e2fcc05c251fb516b08
Valdir Cezar <valdircezar312@gmail.com>
2021-05-03 13:51:45 -0300

    chore: add uuid

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on PORT: 3000");
-})
-
-app.get("/ignite", (req, res) => {
-  return res.json({ message: "Ignite started!" })
 })
